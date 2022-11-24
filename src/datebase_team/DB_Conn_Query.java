c9fddd8edfd5558e86bafc078951cca153e3f278
b-sujin <67819075+b-sujin@users.noreply.github.com>
2022-11-24 22:14:46 +0900

    b-sujin
    수정

--- src/datebase_team/DB_Conn_Query.java
+++ src/datebase_team/DB_Conn_Query.java
@@ -4,7 +4,6 @@ import java.sql.*;
 import java.util.*;
 
 import DTO.shelt_dog;
-import doglist.DB_Conn_Query;
 import DTO.missing;
 import DTO.adopt;
 
@@ -15,7 +14,7 @@ public class DB_Conn_Query {
     Connection conn = null;
     ResultSet rs = null;
     PreparedStatement pstmt = null;
-    Statement stmt = null;
+
 
     String url = "jdbc:oracle:thin:@localhost:1521:XE"; String id = "dog"; String password = "1234";
 
@@ -92,95 +91,72 @@ public class DB_Conn_Query {
     
     
     
-    
-    
-    ////////////////////////수정 중 실행 x
-    //입양 신청 목록 출력 (select - statement + where 조)
-    public static ArrayList<adopt> read_adopt() {
-		ArrayList<adopt> arr = new ArrayList<adopt>();
-		Scanner scan = new Scanner(System.in);
-		
-		System.out.println("입양 신청 목록을 알고 싶은 달을 입력하시오 : ");
-		String month = scan.nextLine();
-        System.out.println(arr);
-        try {
-        	DB_Connect();
-        	String sql = "select * from 입양신청 where 입양신청일자 =" + month;
-            Statement stmt = conn.createStatement();
-            rs = stmt.executeQuery(sql);
-            while (rs.next()) {
-                arr.add(new adopt(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5),
-                		rs.getString(6), rs.getString(7))); 
+
+
+    //입양 신청 목록 출력 (select - statement + where 조건)
+    public Date lastday(String date) {
+    	DB_Connect();
+    	Statement stmt = null;
+    	Date date1 = null;
+    	try {
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("Select last_day('"+date+"') from dual"); //해당 월 막날 sql문 
+            while(rs.next()){
+            	date1 = rs.getDate(1);
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
+    	}catch (SQLException e) {
+			e.printStackTrace();
+    	}finally {
             try {
                 stmt.close();
+                conn.close();               
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+    	}
+		return date1;
+    }
+    
+
+
+    public ArrayList<adopt> read_adopt2() throws ParseException, SQLException {
+		ArrayList<adopt> arr = new ArrayList<adopt>();
+		Statement stmt2 = null;
+		
+		//사용자로부터 입력받기
+    	Scanner scan = new Scanner(System.in);
+		System.out.println("입양 신청 목록을 알고 싶은 년도와 달을 입력하시오(ex. 2020/10) : ");
+		String date = scan.nextLine();
+
+		date = date +"/01";  //2020/10/01 - 해당 년도, 달의 첫 날
+        Date sql2 = lastday(date);  //해당 년도, 달의 막 날 
+    
+		try {
+	        DB_Connect();
+	        
+	        //해당 한 달 내 입양신청 목록 sql문->Select * from 입양신청 where 입양신청일자 between TO_DATE('2022/11/01','YYYY/MM/DD') And TO_DATE('2022/11/30','YYYY/MM/DD');
+        	String sql1 = "Select * from 입양신청 where 입양신청일자 between TO_DATE('"+date+"','YYYY/MM/DD') And TO_DATE('"+sql2+"','YYYY-MM-DD')";
+            stmt2 = conn.createStatement();
+            rs = stmt2.executeQuery(sql1);
+            while (rs.next()) {
+            	//출력 확인용 코드 
+            	//System.out.print(rs.getString("입양신청번호")+"\t"+rs.getDate("입양신청일자")+ "\t"+rs.getString("입양유기견이름")+"\t"+rs.getString("입양유기견품종")+"\t"+
+            			//rs.getString("입양유기견성별")+"\t"+rs.getString("보호기관번호")+"\t"+rs.getString("회원아이디")+"\n");
+            	           	
+                arr.add(new adopt(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5),
+                		rs.getString(6), rs.getString(7))); 
+            }	
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+            try {
+                stmt2.close();
                 conn.close();
+                
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        return arr;
-	}
-    
-    
-    
-
-    
-   
-    ///////////////////////////////////////////////
-    
-    
-    
-
-    public ArrayList<adopt> read_adopt2() {
-		//사용자로부터 값을 입력 받기위함
-
-		ArrayList<adopt> arr = new ArrayList<adopt>();
-		//db작업을 위해 필요한 것들
-		Statement stmt = null;
-		ResultSet rs = null;
-		Connection conn = null;
-		//미리 입력받기
-    	Scanner scan = new Scanner(System.in);
-
-		String month1 = scan.nextLine();
-		
-		SimpleDateFormat formatter = new SimpleDateFormat("yyyy년 MM월 00일");
-		java.util.Date date = formatter.parse(month1);
-		
-		try {
-	        try {
-	        	
-	        	String url = "jdbc:oracle:thin:@localhost:1521:XE"; String id = "dog"; String password = "1234";
-	            conn = DriverManager.getConnection(url, id, password);
-	            System.out.println("DB 연결 성공");
-	        } catch (SQLException e) {
-	            e.printStackTrace();
-	        }
-	        
-        	String sql = "select * from 입양신청 where 입양신청일자=" + date;
-            Statement stmt1 = conn.createStatement();
-            rs = stmt1.executeQuery(sql);
-            while (rs.next()) {
-                arr.add(new adopt(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5),
-                		rs.getString(6), rs.getString(7))); 
-            }
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}finally{
-			//5. 자원 반납 : 만들어진 순서의 역순으로 진행
-			if(rs != null) try {rs.close();} catch (SQLException e2) {}
-			if(stmt != null) try {stmt.close();} catch (SQLException e2) {}
-			if(conn != null) try {conn.close();} catch (SQLException e2) {}
-	  }
 		return arr;
-    }
-    public static void main(String arg[]) throws SQLException, ParseException{
-    	DB_Conn_Query dbconquery = new DB_Conn_Query();
-    	DB_Conn_Query.read_adopt();
     }
 }
