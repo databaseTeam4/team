6e2741a2e90e69caf8390e232a574a5a6331e91f
b-sujin <67819075+b-sujin@users.noreply.github.com>
2022-11-25 21:45:22 +0900

    b-sujin
    프로시저 추가 + 주석 수정

--- src/datebase_team/DB_Conn_Query.java
+++ src/datebase_team/DB_Conn_Query.java
@@ -2,13 +2,15 @@ package datebase_team;
 
 import java.sql.*;
 import java.util.*;
+import java.text.*;
 
 import DTO.shelt_dog;
 import DTO.missing;
 import DTO.adopt;
+import oracle.jdbc.OracleTypes;
 
 import java.sql.Date;
-import java.text.*;
+
 
 public class DB_Conn_Query {
     Connection conn = null;
@@ -18,14 +20,14 @@ public class DB_Conn_Query {
 
     String url = "jdbc:oracle:thin:@localhost:1521:XE"; String id = "dog"; String password = "1234";
 
-////드라이브 적재
+//// 드라이브 적재 ///////////////////////////////////////////////////////////////////////////////////////////////////////
     public DB_Conn_Query() {
     	try { Class.forName("oracle.jdbc.driver.OracleDriver");
     		System.out.println("드라이버 적재 성공");   		
     	} catch(ClassNotFoundException e) {System.out.println("No Driver");}
     }
     
-/////DB 연결 
+///// DB 연결 ///////////////////////////////////////////////////////////////////////////////////////////////////////
     private void DB_Connect() {
         try {
             conn = DriverManager.getConnection(url, id, password);
@@ -36,7 +38,7 @@ public class DB_Conn_Query {
     }
     
     
-    //유기견 테이블 목록 출력 (select - preparedstatment)
+///// 유기견 테이블 목록 출력 (select - preparedstatment) ////////////////////////////////////////////////////////////////
     public ArrayList<shelt_dog> read_shelt_dog() {
 		ArrayList<shelt_dog> arr = new ArrayList<shelt_dog>();
         System.out.println(arr);
@@ -63,7 +65,7 @@ public class DB_Conn_Query {
 	}
     
     
-    //신고 기록 테이블 목록 출력 (select - preparedstatment)
+///// 신고 기록 테이블 목록 출력 (select - preparedstatment) //////////////////////////////////////////////////////////////
     public ArrayList<missing> read_missing() {
 		ArrayList<missing> arr = new ArrayList<missing>();
         System.out.println(arr);
@@ -94,8 +96,8 @@ public class DB_Conn_Query {
 
 ///////////해당 월에 입양 신청 내역이 없을 시 -> 신청 내역이 없다는 문장 추후 저장 프로시저 후 생각해보고 추가하겠습니다.
     
-    //입양 신청 목록 출력 (select - statement + where 조건)
-    // 1. 해당 말일 구하는 함수
+///// 입양 신청 목록 출력 (select - statement + where 조건) ///////////////////////////////////////////////////////////////////////////
+///// 1. 해당 말일 구하는 함수
     public Date lastday(String date) {
     	DB_Connect();
     	Statement stmt = null;
@@ -120,7 +122,7 @@ public class DB_Conn_Query {
     }
     
 
-    // 2.입양목록 출력
+///// 2.입양목록 출력 
     public ArrayList<adopt> read_adopt() throws ParseException, SQLException {
 		ArrayList<adopt> arr = new ArrayList<adopt>();
 		Statement stmt2 = null;
@@ -160,5 +162,34 @@ public class DB_Conn_Query {
             }
         }
 		return arr;
+    }
+    
+    
+///// 저장프로시저(CallableStatement) - P_특정지역유기견목록 ///////////////////////////////////////////////////////////////////////////////////
+    private void p_localsheltdog() { //In parameter + cursor 반환 
+    	try { 
+    		DB_Connect();
+    		CallableStatement cstmt = conn.prepareCall("{call P_특정지역유기견목록(?, ?)}");
+    		
+    		//사용자로부터 지역 입력 받음
+        	Scanner scan = new Scanner(System.in);
+    		System.out.println("유기견 목록을 확인하고 싶은 지역을 입력하시오 : ");
+    		String region = scan.nextLine();
+    		
+    		cstmt.setString(1,region); //In parameter (지역) 
+    		cstmt.registerOutParameter(2,  OracleTypes.CURSOR); //cursor 반환 
+    		cstmt.executeQuery();
+    		
+    		//테이블 받는 커서 
+    		ResultSet rs = (ResultSet)cstmt.getObject(2);
+    		
+    		//해당 지역 유기견 목록 출력 
+    		while(rs.next()) {
+    			System.out.println(rs.getInt(1) +"\t"+ rs.getString(2) + "\t" + rs.getString(3) + "\t" + rs.getString(4) 
+    			+ "\t" + rs.getString(5) + "\t" + rs.getString(6) + "\t" + rs.getString(7) + "\t" + rs.getString(8) 
+    			+ "\t" + rs.getString(9) + "\t" + rs.getString(10));
+    		}
+    		cstmt.close(); conn.close(); rs.close();
+    	}catch (SQLException e) {e.printStackTrace();}
     }
 }
